ba06bd4be19ffee7bec22a0e0bc9414680882b6b
minat0sensei <122515743+minat0sensei@users.noreply.github.com>
2023-01-12 19:18:55 +0600

    create math.js

---
+++ math.js
@@ -0,0 +1,35 @@
+const result = Math.pow(2, 3);
+console.log(result);
+if (result === 8) {
+    console.log('Success!');
+} else {
+    console.log('Failure!');
+}
+
+// Language: javascript
+const num1 = 20;
+const num2 = 30;
+const gap = Math.abs(num2 - num1);
+console.log(gap);
+if (gap < 5) {
+    console.log('Success!');
+} else {
+    console.log('Failure!');
+}
+const number = 3.141619;
+const rounded = Math.round(number);
+console.log(rounded);
+const result2 = Math.ceil(2.00001)
+console.log(result2);
+const result3 = Math.floor(2.99999)
+console.log(result3);
+
+
+
+//random
+// console.log(Math.random());
+const random = Math.round(Math.random() * 10);
+console.log(random);
+
+
+
